f47093f41b58218fd56fef52417dbb52cf9a4cdb
Tamas Marki <tmarki@gmail.com>
2025-03-13 10:17:08 -0600

    Reset migrations

--- prisma/migrations/20250312212326_add_next_billing_date/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "nextBillingDate" TIMESTAMP(3);
--- prisma/migrations/20250313161654_init/migration.sql
+++ prisma/migrations/20250313161654_init/migration.sql
@@ -5,6 +5,11 @@ CREATE TABLE "User" (
     "name" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "stripeCustomerId" TEXT,
+    "subscriptionStatus" TEXT DEFAULT 'free',
+    "subscriptionId" TEXT,
+    "nextBillingDate" TIMESTAMP(3),
+    "credits" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -23,7 +28,7 @@ CREATE TABLE "Session" (
 CREATE TABLE "MagicLink" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "token" TEXT NOT NULL,
+    "csrfToken" TEXT NOT NULL,
     "expires" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -37,7 +42,13 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 CREATE UNIQUE INDEX "Session_token_key" ON "Session"("token");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "MagicLink_token_key" ON "MagicLink"("token");
+CREATE UNIQUE INDEX "MagicLink_csrfToken_key" ON "MagicLink"("csrfToken");
+
+-- CreateIndex
+CREATE INDEX "MagicLink_email_idx" ON "MagicLink"("email");
+
+-- CreateIndex
+CREATE INDEX "MagicLink_expires_idx" ON "MagicLink"("expires");
 
 -- AddForeignKey
 ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
